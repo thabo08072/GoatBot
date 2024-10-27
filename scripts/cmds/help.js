8fb7445c05de2af9e2f7a9dc5a62349bad42535b
thabo080726 <thabosiyabongad@gmail.com>
2024-10-27 19:30:44 +0200

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🎮 | EF-PRIME]";
+const doNotDelete = "[ 🎮 | TSD]";
 /**
 * @author NTKhang
 * @author: do not delete it
