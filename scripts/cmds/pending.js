1d0078065cb41b296e12df4320109f7a037dc668
thabo080726 <thabosiyabongad@gmail.com>
2024-10-31 10:12:44 +0200

    Update pending.js

--- scripts/cmds/pending.js
+++ scripts/cmds/pending.js
@@ -47,7 +47,7 @@ onReply: async function({ api, event, Reply, getLang, commandName, prefix }) {
         const index = body.split(/\s+/);
         for (const singleIndex of index) {
             if (isNaN(singleIndex) || singleIndex <= 0 || singleIndex > Reply.pending.length) return api.sendMessage(getLang("invaildNumber", singleIndex), threadID, messageID);
-            api.sendMessage(`✅ | Connected successfully !\n\n❤ | lance AI Chat Bot 👽`, Reply.pending[singleIndex - 1].threadID);
+            api.sendMessage(`✅ | Connected successfully !\n\n❤ | Whis Angel 😇`, Reply.pending[singleIndex - 1].threadID);
             count+=1;
         }
         return api.sendMessage(getLang("approveSuccess", count), threadID, messageID);
